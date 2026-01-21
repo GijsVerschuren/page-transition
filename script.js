0ab79a0b6ed174f144861fcd296d7b01ce8e022b
GijsVerschuren <153927139+GijsVerschuren@users.noreply.github.com>
2026-01-21 14:22:40 +0100

    heart animation

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const button = document.querySelector(".main");
+const heart = document.querySelector(".heart");
 
 button.addEventListener('click', () => { 
     button.classList.add("active");
+    heart.classList.add("animate");
 });
+
+// heart.addEventListener('click', () => { 
+    
+// });
