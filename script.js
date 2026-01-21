14801ae6f1cbd8e12f9f7f5b745385bcae91b498
GijsVerschuren <153927139+GijsVerschuren@users.noreply.github.com>
2026-01-21 14:49:17 +0100

    lots of hearts
    a lot of hearts

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const button = document.querySelector(".main");
-const heart = document.querySelector(".heart");
+const text = document.querySelector(".text");
 
 button.addEventListener('click', () => { 
-    button.classList.add("active");
-    heart.classList.add("animate");
-});
-
-// heart.addEventListener('click', () => { 
+    button.classList.remove("active");
     
-// });
+    setTimeout(() => {
+        button.classList.add("active");
+    }, 10);
+
+
+    
+    // Create 1000 hearts with delay
+    for (let i = 0; i < 1000; i++) {
+        setTimeout(() => {
+            const heart = document.createElement('div');
+            heart.classList.add('heart', 'animate');
+            heart.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="3000"
+                    width="3200" viewBox="20 -10 315 342">
+                    <defs>
+                        <g id="heart">
+                            <path stroke="red" pathLength="1" stroke-width="5" d="M0 200 v-200 h200 
+            a100,100 90 0,1 0,200
+            a100,100 90 0,1 -200,0
+            z" />
+                        </g>
+                    </defs>
+                    <use xlink:href="#heart" class="outline" fill="none" transform="rotate(225,150,121)" />
+                </svg>
+            `;
+            
+            document.body.appendChild(heart);
+            
+            // Remove heart after animation completes
+            setTimeout(() => {
+                heart.remove();
+            }, 5000);
+        }, i * 1000); // 2 second delay between each heart
+    }
+
+
+});
